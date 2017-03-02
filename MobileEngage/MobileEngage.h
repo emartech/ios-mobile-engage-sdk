a100b052059cf295ed5b55e5d56b28161b9893ea
LaszloOriEmarsys <laszlo.ori@emarsys.com>
2017-03-02 10:29:25 +0100

    Initial commit

---
+++ MobileEngage/MobileEngage.h
@@ -0,0 +1,19 @@
+//
+//  MobileEngage.h
+//  MobileEngage
+//
+//  Created by Laszlo Ori on 2017. 03. 02..
+//  Copyright (c) 2017 Emarsys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+//! Project version number for MobileEngage.
+FOUNDATION_EXPORT double MobileEngageVersionNumber;
+
+//! Project version string for MobileEngage.
+FOUNDATION_EXPORT const unsigned char MobileEngageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileEngage/PublicHeader.h>
+
