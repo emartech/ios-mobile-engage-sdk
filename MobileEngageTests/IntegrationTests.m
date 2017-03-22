72f4123bd83013a311c0b01842d52d2dced59d5b
LaszloOriEmarsys <laszlo.ori@emarsys.com>
2017-03-22 11:05:41 +0100

    test(integration_tests): SUITEDEV-8594 Integration tests have been created for public interface.

---
+++ MobileEngageTests/IntegrationTests.m
@@ -0,0 +1,125 @@
+//
+//  Created by Laszlo Ori on 2017. 03. 22..
+//  Copyright (c) 2017 Emarsys. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "FakeStatusDelegate.h"
+#import "MobileEngage.h"
+#import "MEConfigBuilder.h"
+#import "MEConfig.h"
+
+SPEC_BEGIN(IntegrationTests)
+
+    describe(@"Public interface methods", ^{
+        it(@"should return with eventId, and finish with success for anonymousAppLogin", ^{
+            MEConfig *config = [MEConfig makeWithBuilder:^(MEConfigBuilder *builder) {
+                [builder setCredentialsWithApplicationId:@"14C19-A121F"
+                                       applicationSecret:@"PaNkfOD90AVpYimMBuZopCpm8OWCrREu"];
+            }];
+            [MobileEngage setupWithConfig:config
+                            launchOptions:nil];
+            FakeStatusDelegate *statusDelegate = [FakeStatusDelegate new];
+            [MobileEngage setStatusDelegate:statusDelegate];
+
+            NSString *eventId = [MobileEngage appLogin];
+
+            [[eventId shouldNot] beNil];
+            [[expectFutureValue(@(statusDelegate.successCount)) shouldEventually] equal:@1];
+            [[expectFutureValue(@(statusDelegate.errorCount)) shouldEventually] equal:@0];
+        });
+
+        it(@"should return with eventId, and finish with success for appLogin", ^{
+            MEConfig *config = [MEConfig makeWithBuilder:^(MEConfigBuilder *builder) {
+                [builder setCredentialsWithApplicationId:@"14C19-A121F"
+                                       applicationSecret:@"PaNkfOD90AVpYimMBuZopCpm8OWCrREu"];
+            }];
+            [MobileEngage setupWithConfig:config
+                            launchOptions:nil];
+            FakeStatusDelegate *statusDelegate = [FakeStatusDelegate new];
+            [MobileEngage setStatusDelegate:statusDelegate];
+
+            NSString *eventId = [MobileEngage appLoginWithContactFieldId:@123456789
+                                                       contactFieldValue:@"contactFieldValue"];
+
+            [[eventId shouldNot] beNil];
+            [[expectFutureValue(@(statusDelegate.successCount)) shouldEventually] equal:@1];
+            [[expectFutureValue(@(statusDelegate.errorCount)) shouldEventually] equal:@0];
+        });
+
+        it(@"should return with eventId, and finish with success for trackMessageOpen", ^{
+            MEConfig *config = [MEConfig makeWithBuilder:^(MEConfigBuilder *builder) {
+                [builder setCredentialsWithApplicationId:@"14C19-A121F"
+                                       applicationSecret:@"PaNkfOD90AVpYimMBuZopCpm8OWCrREu"];
+            }];
+            [MobileEngage setupWithConfig:config
+                            launchOptions:nil];
+            FakeStatusDelegate *statusDelegate = [FakeStatusDelegate new];
+            [MobileEngage setStatusDelegate:statusDelegate];
+
+            NSString *eventId = [MobileEngage trackMessageOpenWithUserInfo:@{@"u": @"{\"sid\":\"123456789\"}"}];
+
+            [[eventId shouldNot] beNil];
+            [[expectFutureValue(@(statusDelegate.successCount)) shouldEventually] equal:@1];
+            [[expectFutureValue(@(statusDelegate.errorCount)) shouldEventually] equal:@0];
+        });
+
+        it(@"should return with eventId, and finish with success for trackCustomEvent without attributes", ^{
+            MEConfig *config = [MEConfig makeWithBuilder:^(MEConfigBuilder *builder) {
+                [builder setCredentialsWithApplicationId:@"14C19-A121F"
+                                       applicationSecret:@"PaNkfOD90AVpYimMBuZopCpm8OWCrREu"];
+            }];
+            [MobileEngage setupWithConfig:config
+                            launchOptions:nil];
+            FakeStatusDelegate *statusDelegate = [FakeStatusDelegate new];
+            [MobileEngage setStatusDelegate:statusDelegate];
+
+            NSString *eventId = [MobileEngage trackCustomEvent:@"eventName"
+                                               eventAttributes:nil];
+
+            [[eventId shouldNot] beNil];
+            [[expectFutureValue(@(statusDelegate.successCount)) shouldEventually] equal:@1];
+            [[expectFutureValue(@(statusDelegate.errorCount)) shouldEventually] equal:@0];
+        });
+
+        it(@"should return with eventId, and finish with success for trackCustomEvent with attributes", ^{
+            MEConfig *config = [MEConfig makeWithBuilder:^(MEConfigBuilder *builder) {
+                [builder setCredentialsWithApplicationId:@"14C19-A121F"
+                                       applicationSecret:@"PaNkfOD90AVpYimMBuZopCpm8OWCrREu"];
+            }];
+            [MobileEngage setupWithConfig:config
+                            launchOptions:nil];
+            FakeStatusDelegate *statusDelegate = [FakeStatusDelegate new];
+            [MobileEngage setStatusDelegate:statusDelegate];
+
+            NSString *eventId = [MobileEngage trackCustomEvent:@"eventName"
+                                               eventAttributes:@{
+                                                       @"animal": @"cat",
+                                                       @"drink": @"palinka",
+                                                       @"food": @"pizza"
+                                               }];
+
+            [[eventId shouldNot] beNil];
+            [[expectFutureValue(@(statusDelegate.successCount)) shouldEventually] equal:@1];
+            [[expectFutureValue(@(statusDelegate.errorCount)) shouldEventually] equal:@0];
+        });
+
+        it(@"should return with eventId, and finish with success for appLogout", ^{
+            MEConfig *config = [MEConfig makeWithBuilder:^(MEConfigBuilder *builder) {
+                [builder setCredentialsWithApplicationId:@"14C19-A121F"
+                                       applicationSecret:@"PaNkfOD90AVpYimMBuZopCpm8OWCrREu"];
+            }];
+            [MobileEngage setupWithConfig:config
+                            launchOptions:nil];
+            FakeStatusDelegate *statusDelegate = [FakeStatusDelegate new];
+            [MobileEngage setStatusDelegate:statusDelegate];
+
+            NSString *eventId = [MobileEngage appLogout];
+
+            [[eventId shouldNot] beNil];
+            [[expectFutureValue(@(statusDelegate.successCount)) shouldEventually] equal:@1];
+            [[expectFutureValue(@(statusDelegate.errorCount)) shouldEventually] equal:@0];
+        });
+    });
+
+SPEC_END
