359d582c466586919c09d1f36b9d909dce26c581
pstojcsics <peter.stojcsics@emarsys.com>
2017-04-26 14:49:57 +0200

    chore(release): refine release script + add revoke release script

--- release.sh
+++ release.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-VERSION_NUMBER="$1"
-if [ -z $VERSION_NUMBER ]; then
-  printf "USAGE: \r\n./release <version-number>\n";
-  exit
-else
+function askForSure {
+  while true; do
+      read -p "Did you mean to release '$1' ? " yn
+      case $yn in
+          [Yy]* ) release $1 ; break;;
+          [Nn]* ) exit;;
+          * ) echo "Please answer yes or no.";;
+      esac
+  done
+}
 
+function release {
+  VERSION_NUMBER="$1"
   if GIT_DIR=.git git rev-parse $VERSION_NUMBER >/dev/null 2>&1
   then
       printf "Version tag already exist, exiting...\n"
@@ -13,23 +20,30 @@ else
   fi
 
   printf "Releasing version $VERSION_NUMBER\n";
+
+  printf "#define MOBILEENGAGE_SDK_VERSION @\"$VERSION_NUMBER\"" > MobileEngage/MobileEngageVersion.h
+
+  TEMPLATE="`cat MobileEngageSDK.podspec.template`"
+  PODSPEC="${TEMPLATE/<VERSION_NUMBER>/$VERSION_NUMBER}"
+  printf "$PODSPEC" > MobileEngageSDK.podspec
+
+  git add MobileEngage/MobileEngageVersion.h
+  git add MobileEngageSDK.podspec
+  git commit -m "chore(release): version set to $VERSION_NUMBER"
+  git tag -a "$VERSION_NUMBER" -m "$VERSION_NUMBER"
+
+  git push
+  git push --tags
+
+  pod spec lint
+  pod trunk push MobileEngageSDK.podspec
+
+  printf "[$VERSION_NUMBER] released, go eat some cookies."
+}
+
+if [ -z $1 ]; then
+  printf "USAGE: \r\n./release <version-number>\n";
+  exit
+else
+  askForSure $1
 fi
-
-printf "#define MOBILEENGAGE_SDK_VERSION @\"$VERSION_NUMBER\"" > MobileEngage/MobileEngageVersion.h
-
-TEMPLATE="`cat MobileEngageSDK.podspec.template`"
-PODSPEC="${TEMPLATE/<VERSION_NUMBER>/$VERSION_NUMBER}"
-printf "$PODSPEC" > MobileEngageSDK.podspec
-
-git add MobileEngage/MobileEngageVersion.h
-git add MobileEngageSDK.podspec
-git commit -m "chore(release): version set to $VERSION_NUMBER"
-git tag -a "$VERSION_NUMBER" -m "$VERSION_NUMBER"
-
-git push
-git push --tags
-
-pod spec lint
-pod trunk push MobileEngageSDK.podspec
-
-printf "[$VERSION_NUMBER] released, go eat some cookies."
