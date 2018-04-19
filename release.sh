83539cc3076fe51cd49b965f0e3cbf1446f3d109
stoto <pstojcsics@emarsys.com>
2018-04-19 11:04:01 +0200

    fix(release-script): add remote to pushing tag

--- release.sh
+++ release.sh
@@ -36,7 +36,7 @@ function release {
   git tag -a "$VERSION_NUMBER" -m "$VERSION_NUMBER"
 
   git push
-  git push $VERSION_NUMBER
+  git push origin $VERSION_NUMBER
 
   pod spec lint --allow-warnings
   pod trunk push MobileEngageSDK.podspec --allow-warnings
