740008cc339de9e4dd3e227d036d9be0d456966b
stoto <pstojcsics@emarsys.com>
2018-05-14 10:13:01 +0200

    test(rich-notifications): run rich notification test only on supported platforms SUITEDEV-12356
    Co-authored-by: LaszloOriEmarsys <24588073+LaszloOriEmarsys@users.noreply.github.com>

--- MobileEngageTests/RichNotificationExtension/MEUserNotificationTests.m
+++ MobileEngageTests/RichNotificationExtension/MEUserNotificationTests.m
@@ -6,141 +6,143 @@
 #import <UserNotifications/UNNotificationContent.h>
 
 SPEC_BEGIN(MEUserNotificationTests)
+        if (@available(iOS 10.0, *)) {
 
-        id (^notificationResponseWithUserInfo)(NSDictionary *userInfo) = ^id(NSDictionary *userInfo) {
-            UNNotificationResponse *response = [UNNotificationResponse mock];
-            UNNotification *notification = [UNNotification mock];
-            UNNotificationRequest *request = [UNNotificationRequest mock];
-            UNNotificationContent *content = [UNNotificationContent mock];
-            [response stub:@selector(notification) andReturn:notification];
-            [response stub:@selector(actionIdentifier) andReturn:@"uniqueId"];
-            [notification stub:@selector(request) andReturn:request];
-            [request stub:@selector(content) andReturn:content];
-            [content stub:@selector(userInfo) andReturn:userInfo];
-            return response;
-        };
+            id (^notificationResponseWithUserInfo)(NSDictionary *userInfo) = ^id(NSDictionary *userInfo) {
+                UNNotificationResponse *response = [UNNotificationResponse mock];
+                UNNotification *notification = [UNNotification mock];
+                UNNotificationRequest *request = [UNNotificationRequest mock];
+                UNNotificationContent *content = [UNNotificationContent mock];
+                [response stub:@selector(notification) andReturn:notification];
+                [response stub:@selector(actionIdentifier) andReturn:@"uniqueId"];
+                [notification stub:@selector(request) andReturn:request];
+                [request stub:@selector(content) andReturn:content];
+                [content stub:@selector(userInfo) andReturn:userInfo];
+                return response;
+            };
 
-        describe(@"userNotificationCenter:willPresentNotification:withCompletionHandler:", ^{
+            describe(@"userNotificationCenter:willPresentNotification:withCompletionHandler:", ^{
 
-            it(@"should call the injected delegate's userNotificationCenter:willPresentNotification:withCompletionHandler: method", ^{
-                id userNotificationCenterDelegate = [KWMock mockForProtocol:@protocol(UNUserNotificationCenterDelegate)];
-                UNUserNotificationCenter *mockCenter = [UNUserNotificationCenter mock];
-                UNNotification *mockNotification = [UNNotification mock];
-                void (^ const completionHandler)(UNNotificationPresentationOptions)=^(UNNotificationPresentationOptions options) {
-                };
+                it(@"should call the injected delegate's userNotificationCenter:willPresentNotification:withCompletionHandler: method", ^{
+                    id userNotificationCenterDelegate = [KWMock mockForProtocol:@protocol(UNUserNotificationCenterDelegate)];
+                    UNUserNotificationCenter *mockCenter = [UNUserNotificationCenter mock];
+                    UNNotification *mockNotification = [UNNotification mock];
+                    void (^ const completionHandler)(UNNotificationPresentationOptions)=^(UNNotificationPresentationOptions options) {
+                    };
 
-                [[userNotificationCenterDelegate should] receive:@selector(userNotificationCenter:willPresentNotification:withCompletionHandler:) withArguments:mockCenter, mockNotification, completionHandler];
+                    [[userNotificationCenterDelegate should] receive:@selector(userNotificationCenter:willPresentNotification:withCompletionHandler:) withArguments:mockCenter, mockNotification, completionHandler];
 
-                MEUserNotification *userNotification = [MEUserNotification new];
-                userNotification.delegate = userNotificationCenterDelegate;
+                    MEUserNotification *userNotification = [MEUserNotification new];
+                    userNotification.delegate = userNotificationCenterDelegate;
 
-                [userNotification userNotificationCenter:mockCenter
-                                 willPresentNotification:mockNotification
-                                   withCompletionHandler:completionHandler];
-            });
+                    [userNotification userNotificationCenter:mockCenter
+                                     willPresentNotification:mockNotification
+                                       withCompletionHandler:completionHandler];
+                });
 
-            it(@"should call completion handler with UNNotificationPresentationOptionAlert", ^{
-                MEUserNotification *userNotification = [MEUserNotification new];
-                XCTestExpectation *exp = [[XCTestExpectation alloc] initWithDescription:@"waitForResult"];
-                __block UNNotificationPresentationOptions _option;
-                [userNotification userNotificationCenter:nil
-                                 willPresentNotification:nil
-                                   withCompletionHandler:^(UNNotificationPresentationOptions options) {
-                                       _option = options;
-                                       [exp fulfill];
-                                   }];
+                it(@"should call completion handler with UNNotificationPresentationOptionAlert", ^{
+                    MEUserNotification *userNotification = [MEUserNotification new];
+                    XCTestExpectation *exp = [[XCTestExpectation alloc] initWithDescription:@"waitForResult"];
+                    __block UNNotificationPresentationOptions _option;
+                    [userNotification userNotificationCenter:nil
+                                     willPresentNotification:nil
+                                       withCompletionHandler:^(UNNotificationPresentationOptions options) {
+                                           _option = options;
+                                           [exp fulfill];
+                                       }];
 
-                XCTWaiterResult result = [XCTWaiter waitForExpectations:@[exp] timeout:5];
-                [[theValue(result) should] equal:theValue(XCTWaiterResultCompleted)];
-                [[theValue(_option) should] equal:theValue(UNNotificationPresentationOptionAlert)];
-            });
-
-        });
-
-        describe(@"userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:", ^{
-
-            it(@"should call the injected delegate's userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler: method", ^{
-                id userNotificationCenterDelegate = [KWMock mockForProtocol:@protocol(UNUserNotificationCenterDelegate)];
-                UNUserNotificationCenter *center = [UNUserNotificationCenter nullMock];
-                UNNotificationResponse *notificationResponse = [UNNotificationResponse nullMock];
-                void (^ const completionHandler)()=^{
-                };
-
-                [[userNotificationCenterDelegate should] receive:@selector(userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:) withArguments:center, notificationResponse, completionHandler];
-
-                MEUserNotification *userNotification = [MEUserNotification new];
-                userNotification.delegate = userNotificationCenterDelegate;
-
-                [userNotification userNotificationCenter:center
-                          didReceiveNotificationResponse:notificationResponse
-                                   withCompletionHandler:completionHandler];
-            });
-
-            it(@"should call completion handler", ^{
-                MEUserNotification *userNotification = [MEUserNotification new];
-                XCTestExpectation *exp = [[XCTestExpectation alloc] initWithDescription:@"waitForResult"];
-                [userNotification userNotificationCenter:nil didReceiveNotificationResponse:nil withCompletionHandler:^{
-                    [exp fulfill];
-                }];
-
-                XCTWaiterResult result = [XCTWaiter waitForExpectations:@[exp] timeout:5];
-                [[theValue(result) should] equal:theValue(XCTWaiterResultCompleted)];
-            });
-
-
-            it(@"should call MobileEngage.notification.eventHandler with the defined eventName and payload if the action is type of MEAppEvent", ^{
-                id eventHandlerMock = [KWMock mockForProtocol:@protocol(MEEventHandler)];
-                NSString *eventName = @"testEventName";
-                NSDictionary *payload = @{@"key1": @"value1", @"key2": @"value2", @"key3": @"value3"};
-                [[eventHandlerMock should] receive:@selector(handleEvent:payload:) withArguments:eventName, payload];
-
-                MEUserNotification *userNotification = [MEUserNotification new];
-                userNotification.eventHandler = eventHandlerMock;
-
-                NSDictionary *userInfo = @{@"actions": @{
-                        @"uniqueId": @{
-                                @"title": @"actionTitle",
-                                @"type": @"MEAppEvent",
-                                @"name": eventName,
-                                @"payload": payload
-                        }
-                }};
-
-                XCTestExpectation *exp = [[XCTestExpectation alloc] initWithDescription:@"waitForResult"];
-                [userNotification userNotificationCenter:nil
-                          didReceiveNotificationResponse:notificationResponseWithUserInfo(userInfo)
-                                   withCompletionHandler:^{
-                                       [exp fulfill];
-                                   }];
-                [XCTWaiter waitForExpectations:@[exp] timeout:5];
+                    XCTWaiterResult result = [XCTWaiter waitForExpectations:@[exp] timeout:5];
+                    [[theValue(result) should] equal:theValue(XCTWaiterResultCompleted)];
+                    [[theValue(_option) should] equal:theValue(UNNotificationPresentationOptionAlert)];
+                });
 
             });
 
-            it(@"should not call MobileEngage.notification.eventHandler with the defined eventName and payload if the action is not MEAppEvent type", ^{
-                id eventHandlerMock = [KWMock mockForProtocol:@protocol(MEEventHandler)];
-                [[eventHandlerMock shouldNot] receive:@selector(handleEvent:payload:)];
+            describe(@"userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:", ^{
 
-                MEUserNotification *userNotification = [MEUserNotification new];
-                userNotification.eventHandler = eventHandlerMock;
+                it(@"should call the injected delegate's userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler: method", ^{
+                    id userNotificationCenterDelegate = [KWMock mockForProtocol:@protocol(UNUserNotificationCenterDelegate)];
+                    UNUserNotificationCenter *center = [UNUserNotificationCenter nullMock];
+                    UNNotificationResponse *notificationResponse = [UNNotificationResponse nullMock];
+                    void (^ const completionHandler)()=^{
+                    };
 
-                NSDictionary *userInfo = @{@"actions": @{
-                        @"uniqueId": @{
-                                @"title": @"actionTitle",
-                                @"type": @"someStuff",
-                                @"name": @"testEventName",
-                                @"payload": @{@"key1": @"value1", @"key2": @"value2", @"key3": @"value3"}
-                        }
-                }};
+                    [[userNotificationCenterDelegate should] receive:@selector(userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:) withArguments:center, notificationResponse, completionHandler];
 
-                XCTestExpectation *exp = [[XCTestExpectation alloc] initWithDescription:@"waitForResult"];
-                [userNotification userNotificationCenter:nil
-                          didReceiveNotificationResponse:notificationResponseWithUserInfo(userInfo)
-                                   withCompletionHandler:^{
-                                       [exp fulfill];
-                                   }];
-                [XCTWaiter waitForExpectations:@[exp] timeout:5];
+                    MEUserNotification *userNotification = [MEUserNotification new];
+                    userNotification.delegate = userNotificationCenterDelegate;
 
+                    [userNotification userNotificationCenter:center
+                              didReceiveNotificationResponse:notificationResponse
+                                       withCompletionHandler:completionHandler];
+                });
+
+                it(@"should call completion handler", ^{
+                    MEUserNotification *userNotification = [MEUserNotification new];
+                    XCTestExpectation *exp = [[XCTestExpectation alloc] initWithDescription:@"waitForResult"];
+                    [userNotification userNotificationCenter:nil didReceiveNotificationResponse:nil withCompletionHandler:^{
+                        [exp fulfill];
+                    }];
+
+                    XCTWaiterResult result = [XCTWaiter waitForExpectations:@[exp] timeout:5];
+                    [[theValue(result) should] equal:theValue(XCTWaiterResultCompleted)];
+                });
+
+
+                it(@"should call MobileEngage.notification.eventHandler with the defined eventName and payload if the action is type of MEAppEvent", ^{
+                    id eventHandlerMock = [KWMock mockForProtocol:@protocol(MEEventHandler)];
+                    NSString *eventName = @"testEventName";
+                    NSDictionary *payload = @{@"key1": @"value1", @"key2": @"value2", @"key3": @"value3"};
+                    [[eventHandlerMock should] receive:@selector(handleEvent:payload:) withArguments:eventName, payload];
+
+                    MEUserNotification *userNotification = [MEUserNotification new];
+                    userNotification.eventHandler = eventHandlerMock;
+
+                    NSDictionary *userInfo = @{@"actions": @{
+                            @"uniqueId": @{
+                                    @"title": @"actionTitle",
+                                    @"type": @"MEAppEvent",
+                                    @"name": eventName,
+                                    @"payload": payload
+                            }
+                    }};
+
+                    XCTestExpectation *exp = [[XCTestExpectation alloc] initWithDescription:@"waitForResult"];
+                    [userNotification userNotificationCenter:nil
+                              didReceiveNotificationResponse:notificationResponseWithUserInfo(userInfo)
+                                       withCompletionHandler:^{
+                                           [exp fulfill];
+                                       }];
+                    [XCTWaiter waitForExpectations:@[exp] timeout:5];
+
+                });
+
+                it(@"should not call MobileEngage.notification.eventHandler with the defined eventName and payload if the action is not MEAppEvent type", ^{
+                    id eventHandlerMock = [KWMock mockForProtocol:@protocol(MEEventHandler)];
+                    [[eventHandlerMock shouldNot] receive:@selector(handleEvent:payload:)];
+
+                    MEUserNotification *userNotification = [MEUserNotification new];
+                    userNotification.eventHandler = eventHandlerMock;
+
+                    NSDictionary *userInfo = @{@"actions": @{
+                            @"uniqueId": @{
+                                    @"title": @"actionTitle",
+                                    @"type": @"someStuff",
+                                    @"name": @"testEventName",
+                                    @"payload": @{@"key1": @"value1", @"key2": @"value2", @"key3": @"value3"}
+                            }
+                    }};
+
+                    XCTestExpectation *exp = [[XCTestExpectation alloc] initWithDescription:@"waitForResult"];
+                    [userNotification userNotificationCenter:nil
+                              didReceiveNotificationResponse:notificationResponseWithUserInfo(userInfo)
+                                       withCompletionHandler:^{
+                                           [exp fulfill];
+                                       }];
+                    [XCTWaiter waitForExpectations:@[exp] timeout:5];
+
+                });
             });
-        });
+        }
 
 SPEC_END
