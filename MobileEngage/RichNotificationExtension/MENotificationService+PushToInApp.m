7ba07cb1c44cb60e4c4c4b0edc8b79d7a831cea2
LaszloOriEmarsys <laszlo.ori@emarsys.com>
2018-07-19 15:51:56 +0200

    fix(pod): fix import for lint SUITEDEV-13507

--- MobileEngage/RichNotificationExtension/MENotificationService+PushToInApp.m
+++ MobileEngage/RichNotificationExtension/MENotificationService+PushToInApp.m
@@ -1,9 +1,10 @@
 //
 // Copyright (c) 2018 Emarsys. All rights reserved.
 //
+#import <CoreSDK/EMSDictionaryValidator.h>
 #import "MENotificationService.h"
 #import "MENotificationService+PushToInApp.h"
-#import "EMSDictionaryValidator.h"
+
 #import "MEDownloader.h"
 
 @implementation MENotificationService (PushToInApp)
